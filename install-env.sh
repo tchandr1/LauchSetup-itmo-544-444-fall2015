a5a37694891ee553cf0ced6d2e6a6cc84bf4015f
thanusha chandrahasa <tchandr1@hawk.iit.edu>
2015-10-04 20:11:30 -0500

    Changed shell script files

--- install-env.sh
+++ install-env.sh
@@ -8,3 +8,5 @@ git clone https://github.com/tchandr1/LauchSetup-itmo-544-444-fall2015.git
 mv ./LauchSetup-itmo-544-444-fall2015/images /var/www/html/images
 mv ./LauchSetup-itmo-544-444-fall2015/index.html /var/www/html/
 mv ./LauchSetup-itmo-544-444-fall2015/page2.html /var/www/html/
+
+echo "Hello! I am Thanusha Chandrahasa. Course:ITMO544-444, Week05Assignment" > /tmp/hello.txt
--- launch.sh
+++ launch.sh
@@ -3,5 +3,4 @@
 
 echo “LaunchSetup”
 
-aws ec2 run-instances --image-id ami-$1 --count $2 --instance-type $3 --key-name $6 --security-group-ids  $4 --subnet-id $5 --associate-public-ip-address --user-data file://LaunchSetup-itmo-544-444-fall2015/install-env.sh --debug
-
+aws ec2 run-instances --image-id ami-$1 --count $2 --instance-type $3 --key-name $6 --security-group-ids  $4 --subnet-id $5 --associate-public-ip-address --user-data file://install-env.sh --debug
