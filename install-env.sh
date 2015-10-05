fbd0efc8c2259c7737f8593b6ed58222aa51a7a3
thanusha chandrahasa <tchandr1@hawk.iit.edu>
2015-10-04 20:04:01 -0500

    All files added

---
+++ install-env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y apache2 git
+
+git clone https://github.com/tchandr1/LauchSetup-itmo-544-444-fall2015.git
+
+mv ./LauchSetup-itmo-544-444-fall2015/images /var/www/html/images
+mv ./LauchSetup-itmo-544-444-fall2015/index.html /var/www/html/
+mv ./LauchSetup-itmo-544-444-fall2015/page2.html /var/www/html/
