62278553d4a55741fe406db22417ba8282f6e3f5
thanusha chandrahasa <tchandr1@hawk.iit.edu>
2015-10-04 19:43:58 -0500

    Initial launch

---
+++ launch.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+
+echo “LaunchSetup”
+
+aws ec2 run-instances --image-id ami-$1 --count $2 --instance-type $3 --key-name $6 --security-group-ids  $4 --subnet-id $5 --associate-public-ip-address --user-data file://ApplicationSetup-itmo-544-444-fall2015/install-env.sh --debug
+
+
+
+
+
+
